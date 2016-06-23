87286a02e905745ec5e82b90815f4ef4b49bc989
Rami Nasser <ramin@liveperson.com>
2016-06-23 15:42:24 +0300

    final problem 17

--- src/Problem17/digitsCont.java
+++ src/Problem17/digitsCont.java
@@ -1,26 +1,43 @@
 package Problem17;
 
-import java.util.Map;
-
 public class digitsCont {
 
-	int[] digits = new int[4];
-	int letterTotal = 0;
-	Map<int, int> numToWord= new HashMap();
-	
+	int[] digits = new int[3];
 
-	
+	int[][] map = { { 0, 13, 13, 15, 14, 14, 13, 15, 15, 14 }, { 0, 3, 6, 6, 5, 5, 5, 7, 6, 6 },
+			{ 0, 3, 3, 5, 4, 4, 3, 5, 5, 4 }, { 0, 6, 6, 8, 8, 7, 7, 9, 8, 8 } };
+
+	int letterTotal = 0;
 
 	public digitsCont(int number) {
+
 		super();
 		this.digSeprator(number);
-		
+
+		if (digits[1] == 1 && digits[2] > 0) {
+
+			letterTotal += map[3][digits[2]];
+			letterTotal += map[0][digits[0]];
+
+		} else {
+			for (int i = 0; i < digits.length; i++) {
+
+				letterTotal += map[i][digits[i]];
+
+			}
+
+		}
+
+	}
+
+	public int getLetterTotal() {
+		return letterTotal;
 	}
 
 	private void digSeprator(int number) {
 
 		int num = number;
-		int i = 3;
+		int i = 2;
 		while (i >= 0) {
 
 			this.digits[i] = num % 10;
@@ -34,7 +51,5 @@ public class digitsCont {
 	public int[] getDigits() {
 		return digits;
 	}
-	
-	
 
 }
--- src/Problem17/main.java
+++ src/Problem17/main.java
@@ -2,4 +2,17 @@ package Problem17;
 
 public class main {
 
+	public static void main(String[] args) {
+
+		int total = 0;
+
+		for (int i = 1; i <= 999; i++) {
+
+			digitsCont test = new digitsCont(i);
+			total += test.getLetterTotal();
+		}
+
+		System.out.println(total - 16);
+
+	}
 }
